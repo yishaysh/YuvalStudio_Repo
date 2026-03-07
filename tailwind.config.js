370c081db9f768ce6aa9c0a2e1ce7138acde83f8
Yishay Shavlev <yishay.shavlev@made4net.com>
2026-03-07 23:56:11 +0200

    fix(ui): Desktop navigation menu visibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,11 @@
 export default {
   content: [
     "./index.html",
+    "./*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./contexts/**/*.{js,ts,jsx,tsx}"
   ],
   darkMode: 'class',
   theme: {
